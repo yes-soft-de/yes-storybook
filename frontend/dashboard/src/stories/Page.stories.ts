1b27fbd59e449fa9552d4c665a514b9d01897c01
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-05-22 20:33:03 +0300

    fixing missing component

--- frontend/dashboard/src/stories/Page.stories.ts
+++ frontend/dashboard/src/stories/Page.stories.ts
@@ -2,25 +2,36 @@ import { moduleMetadata } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
 // @ts-ignore
 import { Story, Meta } from '@storybook/angular/types-6-0';
-import Page from './page.component';
 
 import * as HeaderStories from './Header.stories';
-import {ButtonComponent} from '../app/shared/ui/component/button/button.component';
+import { ButtonComponent } from '../app/shared/ui/component/button/button.component';
 import { YesNavComponent } from '../app/navigation/ui/yes-nav/yes-nav.component';
+import { PageComponent } from '../app/navigation/ui/page/page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export default {
-  title: 'Example/Page',
+  title: 'Assembly/Page',
   component: YesNavComponent,
   decorators: [
     moduleMetadata({
       declarations: [ButtonComponent, YesNavComponent],
-      imports: [CommonModule],
+      imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule],
     }),
   ],
 } as Meta;
 
-const Template: Story<Page> = (args: Page) => ({
-  component: Page,
+const Template: Story<PageComponent> = (args: PageComponent) => ({
+  component: PageComponent,
   props: args,
 });
 
